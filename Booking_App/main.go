8ad6c3d61d7262ed529a57bc5d699cf9e59b18ca
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-05-19 19:31:34 +0100

    data types in Go, string,int, uint, etc

--- Booking_App/main.go
+++ Booking_App/main.go
@@ -6,12 +6,22 @@ import "fmt"
 // an entry point for the go program
 func main() {
 	var conferenceName = "Go Conference" // variable in Go
-	const conferenceTickets = 50  // constants in go
-	var remainingTickets = 50
+	const conferenceTickets int = 50  // constants in go
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("conferenceTicket is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	// using printf
+	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still availalbe\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 
+	// ask user for their name
+	var userName string
+	var userTickets int
+	
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
